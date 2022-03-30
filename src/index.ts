f844bb01aa8bc2e08a97aa33a9fb6a534203c55b
Andrei Kulik <andry_teck@mail.ru>
2022-03-30 19:07:41 +0300

    fix port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import express, { Request, Response } from 'express'
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 app.use(cors())
 app.use(bodyParser());
 
